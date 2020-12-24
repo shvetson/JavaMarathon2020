18c664d6cfe129206d23af9bef0406038e13ca27
Oleg Shvets <shvetson@gmail.com>
2020-12-24 21:25:43 +0300

    Add day 1 tasks solutions

--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -1,7 +1,16 @@
 package day1;
 
+import java.util.Scanner;
+
 public class Task6 {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a number (1-9): ");
+        int k = in.nextInt();
 
+        for (int i = 1; i < 10; i++) {
+            System.out.printf("%d x %d = %d%n", i, k, i * k);
+        }
+        in.close();
     }
 }
