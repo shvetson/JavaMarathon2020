f7a47a5cf8c07e5191bd9ba88d6d9584dd463445
macbook <shvetson@gmail.com>
2021-01-02 10:05:36 +0300

    Add day8 tasks solutions

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,26 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        String stringConcat = "";
+        StringBuilder stringBuilder = new StringBuilder();
+        int counter = 20000;
+        long start = 0;
+        long end = 0;
 
+        // Concat method
+        start = System.nanoTime();
+        for (int i = 0; i <= counter; i++) {
+            stringConcat = stringConcat + i + " ";
+        }
+        end = System.nanoTime();
+        System.out.printf("Time of concat method %d ns\n", end - start);
+
+        // StringBuilder
+        start = System.nanoTime();
+        for (int i = 0; i <= counter; i++) {
+            stringBuilder.append(i).append(" ");
+        }
+        end = System.nanoTime();
+        System.out.printf("Time of StringBuilder %d ns\n", end - start);
     }
 }
